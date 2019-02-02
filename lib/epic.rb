75d96c123cd6fb8c747857fe82737429fc892cb6
Roelof Reitsma <roelof.reitsma@jumbo.com>
2019-02-02 19:23:06 +0100

    Add epic and display epic name in output

---
+++ lib/epic.rb
@@ -0,0 +1,13 @@
+class Epic
+  attr_reader :key, :summary, :id, :name
+
+  def initialize(key, summary, id, name)
+    @key, @summary, @id, @name = key, summary, id, name
+  end
+
+  def self.from_json(json)
+    new(json['key'], json['summary'],
+      json['id'], json['name']
+    )
+  end
+end
--- lib/issue.rb
+++ lib/issue.rb
@@ -1,19 +1,24 @@
+require_relative 'epic'
+
 class Issue
   attr_reader :key, :summary, :id, :source, :estimation, :resolution_date
+  attr_accessor :epic
 
   def initialize(key, summary, id, estimation, resolution_date, source)
     @key, @summary, @id, @estimation, @resolution_date, @source = key, summary, id, estimation, resolution_date, source
   end
 
   def self.from_json(json)
-    new(json['key'], json['fields']['summary'],
+    issue = new(json['key'], json['fields']['summary'],
       json['id'], json['fields']['customfield_10014'] || 0,
       json['fields']['resolutiondate'],
       json
     )
+    issue.epic = Epic.from_json(json['fields']['epic']) if json['fields']['epic']
+    issue
   end
 
   def to_s
-    "Key: #{key}, summary: #{summary}, estimation: #{estimation}"
+    "Key: #{key}, summary: #{summary}, estimation: #{estimation}, epic: #{epic&.name}"
   end
 end
