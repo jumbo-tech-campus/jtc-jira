10bcb479f2c14e7d492ce046a4795050bd63b98f
Roelof Reitsma <roelof.reitsma@jumbo.com>
2019-02-09 17:01:01 +0100

    Make teams index page the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'teams#index'
   resources :teams
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'sprint_report/last_sprint', to: 'sprint_report#last_sprint', as: :last_sprint_report
