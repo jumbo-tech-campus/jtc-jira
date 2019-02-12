9d3dd27da7e40659c77c4aeccb3ac8629bcacecf
Roelof Reitsma <roelof.reitsma@jumbo.com>
2019-02-12 01:22:16 +0100

    Add route to refresh the data for a board from the application

--- app/controllers/sprint_report_controller.rb
+++ app/controllers/sprint_report_controller.rb
@@ -10,6 +10,11 @@ class SprintReportController < ApplicationController
     @sprint = Repository.for(:sprint).find_by(id: params[:id].to_i, board: @board)
   end
 
+  def refresh_data
+    CacheService.new.refresh_team_data(@board.team)
+    redirect_to action: 'sprint', board_id: @board.id, id: @board.last_closed_sprint.id
+  end
+
   private
   def set_board
     @board = Repository.for(:board).find(params[:board_id].to_i)
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :teams
   get 'sprint_report/last_sprint', to: 'sprint_report#last_sprint', as: :last_sprint_report
   get 'sprint_report/sprint', to: 'sprint_report#sprint', as: :sprint_report
+  post 'sprint_report/refresh_data', to: 'sprint_report#refresh_data', as: :sprint_report_refresh
 end
