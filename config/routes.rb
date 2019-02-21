7aadf02159874fbcf97a72004b75d71c820f7237
Roelof Reitsma <roelof.reitsma@jumbo.com>
2019-02-21 13:43:27 +0100

    Fix incorrect root path setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'teams#index'
+
   scope :jira do
-    root 'teams#index'
     resources :teams
     get 'sprint_report/last_sprint', to: 'sprint_report#last_sprint', as: :last_sprint_report
     get 'sprint_report/sprint', to: 'sprint_report#sprint', as: :sprint_report
