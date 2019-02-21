08a57286015226e7e0e9dc14b8a583f2e6b7c6c2
Roelof Reitsma <roelof.reitsma@jumbo.com>
2019-02-21 13:50:13 +0100

    Revert back to routes without scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   root 'teams#index'
 
-  scope :jira do
-    resources :teams
-    get 'sprint_report/last_sprint', to: 'sprint_report#last_sprint', as: :last_sprint_report
-    get 'sprint_report/sprint', to: 'sprint_report#sprint', as: :sprint_report
-    post 'sprint_report/refresh_data', to: 'sprint_report#refresh_data', as: :sprint_report_refresh
-  end
+  resources :teams
+  get 'sprint_report/last_sprint', to: 'sprint_report#last_sprint', as: :last_sprint_report
+  get 'sprint_report/sprint', to: 'sprint_report#sprint', as: :sprint_report
+  post 'sprint_report/refresh_data', to: 'sprint_report#refresh_data', as: :sprint_report_refresh
 end
