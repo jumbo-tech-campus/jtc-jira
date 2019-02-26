2d5d256a6cfba6d3767cc2c58a3e84d1a54914fa
Roelof Reitsma <roelof.reitsma@jumbo.com>
2019-02-26 11:54:59 +0100

    Add buttons for week switching in portfolio report
    However, it doesn't work yet

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,9 +1,9 @@
 class ReportController < ApplicationController
-  before_action :set_dates
 
   def portfolio
     @table = PortfolioReportService.new.last_closed_sprint
     @report_type = "Portfolio"
+    @dates = set_dates.reverse
   end
 
   private
@@ -12,7 +12,8 @@ class ReportController < ApplicationController
     week_number = week_number - 1 if week_number % 2 != 0
 
     begin_date = DateTime.commercial(DateTime.now.year, week_number)
-    @dates = []
-    5.times { |count| @dates << (begin_date - (count * 2).weeks) }
+    dates = []
+    5.times { |count| dates << (begin_date - (count * 2).weeks) }
+    dates
   end
 end
