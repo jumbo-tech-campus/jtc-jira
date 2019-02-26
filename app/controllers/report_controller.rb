9bb9570dbf53c62f93386460b08a10c979712dde
Roelof Reitsma <roelof.reitsma@jumbo.com>
2019-02-26 21:57:19 +0100

    Download portfolio reports as CSV file

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -3,8 +3,13 @@ class ReportController < ApplicationController
   def portfolio
     @report_type = "Portfolio"
     @dates = set_dates.reverse
-    @selected_date = params[:date].to_datetime || @dates.last
+    @selected_date = params[:date]&.to_datetime || @dates.last
     @table = PortfolioReportService.for_sprints_on(@selected_date)
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data to_csv(@table), filename: "portfolio_report_week_#{@selected_date.cweek}_#{@selected_date.cweek + 1}.csv" }
+    end
   end
 
   private
@@ -16,5 +21,13 @@ class ReportController < ApplicationController
     dates = []
     5.times { |count| dates << (begin_date - (count * 2).weeks) }
     dates
+  end
+
+  def to_csv(table)
+    CSV.generate(headers: true) do |csv|
+      table.each do |row|
+        csv << row
+      end
+    end
   end
 end
