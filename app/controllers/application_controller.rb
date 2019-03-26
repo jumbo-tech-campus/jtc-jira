cbaa0fd7e2f3f5c73fa0242924e5e7fccee600e5
Roelof Reitsma <roelof.reitsma@jumbo.com>
2019-03-26 08:43:45 +0100

    Sort teams by name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   private
   def set_teams
-    @teams = Repository.for(:team).all
+    @teams = Repository.for(:team).all.sort_by(&:name)
     @scrum_teams = @teams.select{ |team| team.is_scrum_team? }
   end
 
