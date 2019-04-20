c2e4e2e9648c4fa759b0dc40360a5b200b5b84ce
Roelof Reitsma <roelof.reitsma@jumbo.com>
2019-04-20 16:56:22 +0200

    Fix crash when caching parent epics without wbso project

--- app/factories/parent_epic_factory.rb
+++ app/factories/parent_epic_factory.rb
@@ -1,6 +1,6 @@
 class ParentEpicFactory
   def create_from_jira(jira_epic)
-    wbso_project = jira_epic.fields['customfield_12834']['value'] if jira_epic.fields['customfield_12834']['id'] != '11436'
+    wbso_project = jira_epic.fields['customfield_12834']['value'] if jira_epic.fields['customfield_12834'] && jira_epic.fields['customfield_12834']['id'] != '11436'
     ParentEpic.new(jira_epic.id.to_i, jira_epic.key, jira_epic.summary, wbso_project)
   end
 
