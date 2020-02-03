d5b4be7a1e923ad4943e3627c228c049ec64d629
Roelof Reitsma <roelof.reitsma@jumbo.com>
2020-02-03 23:12:03 +0100

    Fix bug where boards instead of teams were sorted by name

--- app/controllers/department_report_controller.rb
+++ app/controllers/department_report_controller.rb
@@ -5,7 +5,7 @@ class DepartmentReportController < ApplicationController
     department_id = params[:department_id] || '1'
     @department = Repository.for(:department).find(department_id.to_i)
 
-    boards = @department.active_teams.map(&:board).compact.sort(&:name)
+    boards = @department.active_teams.sort(&:name).map(&:board).compact
     @report = CycleTimeOverviewReportService.new(boards, DateTime.new(2019, 1, 1), DateTime.now, 1.year).report(true)
   end
 end
