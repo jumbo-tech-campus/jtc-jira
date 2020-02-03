6be79c126af1917b5e00a12cac74b4fa5b080070
Roelof Reitsma <roelof.reitsma@jumbo.com>
2020-02-03 23:52:42 +0100

    Add cache path improvement to fix caching for department report

--- app/controllers/department_report_controller.rb
+++ app/controllers/department_report_controller.rb
@@ -1,5 +1,5 @@
 class DepartmentReportController < ApplicationController
-  caches_action :cycle_time_overview, expires_in: 12.hours
+  caches_action :cycle_time_overview, expires_in: 12.hours, cache_path: :department_cache_path
 
   def cycle_time_overview
     department_id = params[:department_id] || '1'
@@ -7,5 +7,10 @@ class DepartmentReportController < ApplicationController
 
     boards = @department.active_teams.sort_by(&:name).map(&:board).compact
     @report = CycleTimeOverviewReportService.new(boards, DateTime.new(2019, 1, 1), DateTime.now, 1.year).report(true)
+  end
+
+  protected
+  def department_cache_path
+    { :department_id: params[:department_id] }
   end
 end
