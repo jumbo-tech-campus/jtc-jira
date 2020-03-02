8510310aa2648ef10401f42d53721fdabf26518f
Roelof Reitsma <roelof.reitsma@jumbo.com>
2020-03-02 21:37:09 +0100

    Always show last week by default on the deployment report

--- app/controllers/deployment_report_controller.rb
+++ app/controllers/deployment_report_controller.rb
@@ -1,5 +1,5 @@
 class DeploymentReportController < ApplicationController
-  before_action :set_year_dates
+  before_action :set_last_week_dates
 
   def overview
     @report = DeploymentReportService.new(@start_date, @end_date).deployment_report
@@ -9,5 +9,11 @@ class DeploymentReportController < ApplicationController
       format.csv { send_data to_csv(@report[:issues_table]), filename: "deployment_project_report.csv" }
       format.json { send_data @report.to_json }
     end
+  end
+
+  private
+  def set_last_week_dates
+    @end_date = ApplicationHelper.safe_parse(params[:end_date]) || DateTime.now.beginning_of_week - 1.week
+    @start_date = ApplicationHelper.safe_parse(params[:start_date]) || DateTime.now.end_of_week - 1.week
   end
 end
