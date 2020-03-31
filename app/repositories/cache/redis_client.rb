208668bf17b36bb71b179c075936918c2b022e6d
Roelof Reitsma <roelof.reitsma@jumbo.com>
2020-03-31 21:21:15 +0200

    Stop sending metrics for Redis requests - it's not needed atm

--- app/repositories/cache/redis_client.rb
+++ app/repositories/cache/redis_client.rb
@@ -10,43 +10,23 @@ module Cache
       else
         @client = Redis.new(host: ENV['REDIS_HOST'], db: 0)
       end
-
-      @statsd_client = StatsdClient.new
     end
 
     def get(key)
-      started = Time.now
-      result = "result:success"
-
       begin
         @client.get(key)
       rescue StandardError => e
-        result = "result:failure"
         Rails.logger.error("Error in Redis get. \nRedis client: #{@client.inspect}\n#{e.backtrace}")
         raise
-      ensure
-        @statsd_client.timing('cache.duration',
-          (Time.now - started) * 1000,
-          tags: ["action:get", result, "key:#{key}"]
-        )
       end
     end
 
     def set(key, value)
-      started = Time.now
-      result = "result:success"
-
       begin
         @client.set(key, value)
       rescue StandardError => e
-        result = "result:failure"
         Rails.logger.error("Error in Redis get. \nRedis client: #{@client.inspect}\n#{e.backtrace}")
         raise
-      ensure
-        @statsd_client.timing('cache.duration',
-          (Time.now - started) * 1000,
-          tags: ["action:set", result, "key:#{key}"]
-        )
       end
     end
 
