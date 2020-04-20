e054565754f0f8cce92a5ce939b529ab1372ac3a
Roelof Reitsma <roelof.reitsma@jumbo.com>
2020-04-20 11:38:49 +0200

    Add ID to quarters so that we can reference them easily from kpi goals

--- app/factories/quarter_factory.rb
+++ app/factories/quarter_factory.rb
@@ -1,9 +1,9 @@
 class QuarterFactory
   def create_from_json(json)
-    Quarter.new(json['start_week'], json['end_week'], json['fix_version'], json['year'].to_i)
+    Quarter.new(json['id'], json['start_week'], json['end_week'], json['fix_version'], json['year'].to_i)
   end
 
   def create_from_hash(hash)
-    Quarter.new(hash[:start_week], hash[:end_week], hash[:fix_version], hash[:year])
+    Quarter.new(hash[:id], hash[:start_week], hash[:end_week], hash[:fix_version], hash[:year])
   end
 end
