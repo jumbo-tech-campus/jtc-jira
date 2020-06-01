48d9ebbc59bff1d5470b0a8a05e93a640703420f
Roelof Reitsma <roelof.reitsma@jumbo.com>
2020-06-01 10:17:24 +0200

    Also serialize subteam and components for subclasses of Incident

--- app/serializers/alert_serializer.rb
+++ app/serializers/alert_serializer.rb
@@ -1,6 +1,7 @@
 class AlertSerializer < ActiveModel::Serializer
   attributes :id, :key, :summary, :estimation, :created, :status,
              :resolution_date, :in_progress_date, :release_date, :pending_release_date,
-             :assignee, :resolution, :done_date, :labels, :class_name,
+             :assignee, :resolution, :done_date, :labels, :class_name, :subteam,
+             :components,
              :event_key, :alerted_at
 end
--- app/serializers/incident_serializer.rb
+++ app/serializers/incident_serializer.rb
@@ -1,6 +1,7 @@
 class IncidentSerializer < ActiveModel::Serializer
   attributes :id, :key, :summary, :estimation, :created, :status,
              :resolution_date, :in_progress_date, :release_date, :pending_release_date,
-             :assignee, :resolution, :done_date, :labels, :class_name,
+             :assignee, :resolution, :done_date, :labels, :class_name, :subteam,
+             :components,
              :start_date, :end_date, :reported_date, :causes
 end
