daf5934dfe5bda40d2be45f5ddea1540d3461304
Roelof Reitsma <roelof.reitsma@jumbo.com>
2020-06-01 18:52:11 +0200

    Fix error in KanbanSprint where team was referenced instead of board

--- app/models/kanban_sprint.rb
+++ app/models/kanban_sprint.rb
@@ -1,6 +1,6 @@
 class KanbanSprint < Sprint
   def issues
-    @issues ||= team.issues.select do |issue|
+    @issues ||= board.issues.select do |issue|
       issue.release_date&.between?(start_date, end_date.end_of_day)
     end
   end
